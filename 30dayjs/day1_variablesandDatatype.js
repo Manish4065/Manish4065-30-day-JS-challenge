e88aa0262f171e1c510e4230175ec536b04de068
Manish4065 <109040227+Manish4065@users.noreply.github.com>
2024-07-13 10:31:58 +0000

    variables and datatypes

---
+++ 30dayjs/day1_variablesandDatatype.js
@@ -0,0 +1,24 @@
+// tasks
+var varName=100
+console.log(Math.log(varName));
+
+let abc="string"
+console.log(abc);
+
+const boolvalue=true;
+console.log(boolvalue)
+
+const arrayName=[1,2,3,4];
+const objectName={name:"manish",
+    userId:2021021043
+}
+
+console.log(varName,typeof varName )
+console.log(typeof abc)
+console.log(typeof(abc))
+console.log(typeof boolvalue)
+console.log(typeof arrayName)
+console.log(typeof objectName)
+
+boolvalue=false
+console.log(boolvalue);  // TypeError: Assignment to constant variable.
